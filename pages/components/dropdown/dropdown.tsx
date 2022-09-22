b375a9594394edc798ce375177822dce12277395
AlzahedOudini <stevealzahedoudini@gmail.com>
2022-09-22 06:58:16 +0200

    Corrections des bugs

--- pages/components/dropdown/dropdown.tsx
+++ pages/components/dropdown/dropdown.tsx
@@ -3,13 +3,13 @@ import React, {useState}from 'react';
 
 
 const  Dropdown =() =>{
-    const [showOption, setShowOption] = useState(false);
-    const handleClick = () =>{
-        setShowOption(!showOption);
-    }
+    // const [showOption, setShowOption] = useState(false);
+    // const handleClick = () =>{
+    //     setShowOption(!showOption);
+    // }
   return (
     <div>
-        <div className="relative inline-block text-left">
+        {/* <div className="relative inline-block text-left">
             <div>
                 <button onClick={handleClick}
                  type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 " id="menu-button" aria-expanded="true" aria-haspopup="true">
@@ -21,7 +21,7 @@ const  Dropdown =() =>{
             </div>
 
             {showOption && (
-                <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
+                <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button">
                 <div className="py-1" role="none">
                 <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-0">Cameroun</a>
                 <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-1">Canada</a>
@@ -30,7 +30,7 @@ const  Dropdown =() =>{
                 </div>
             </div>
             ) }
-        </div>
+        </div> */}
     </div>
   )
 }
