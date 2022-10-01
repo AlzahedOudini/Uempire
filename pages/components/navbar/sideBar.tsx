0711619eff78d7bd1e0b809e51b38f79967a28ab
AlzahedOudini <stevealzahedoudini@gmail.com>
2022-10-02 00:10:02 +0200

    update sidebar

--- pages/components/navbar/sideBar.tsx
+++ pages/components/navbar/sideBar.tsx
@@ -17,38 +17,38 @@ function SideBar(props: Props) {
 
        <div className="content_side flex justify-between flex-col sm:flex-row md:flex-row lg:flex-row 2xl:flex-row">
         <div className="boxMenu p-10">
-            <ul className=' grid gap-3 lg:gap-5'>
+            <ul className=' grid gap-3 lg:gap-5 '>
                 <Link href="/">
-                  <li className=' text-2xl sm:text-4xl lg:text-4xl hover:text-[#FFB040] transition-all duration-700 cursor-pointer font-semibold '>Accueil</li>
+                  <li className=' text-2xl text-white sm:text-4xl lg:text-4xl hover:text-[#FFB040] transition-all duration-700 cursor-pointer font-semibold '>Accueil</li>
                 </Link>
                 <Link href="/about">
-                  <li className=' text-2xl sm:text-4xl lg:text-4xl hover:text-[#FFB040] transition-all duration-700 cursor-pointer font-semibold '>A Propos</li>
+                  <li className=' text-2xl text-white sm:text-4xl lg:text-4xl hover:text-[#FFB040] transition-all duration-700 cursor-pointer font-semibold '>A Propos</li>
                 </Link>
                 <Link href="/services">
-                  <li className=' text-2xl sm:text-4xl lg:text-4xl hover:text-[#FFB040] transition-all duration-700 cursor-pointer font-semibold '>Services</li>
+                  <li className=' text-2xl text-white sm:text-4xl lg:text-4xl hover:text-[#FFB040] transition-all duration-700 cursor-pointer font-semibold '>Services</li>
                 </Link>
                 <Link href="/project">
-                  <li className=' text-2xl sm:text-4xl lg:text-4xl hover:text-[#FFB040] transition-all duration-700 cursor-pointer font-semibold '>Projets</li>
+                  <li className=' text-2xl text-white sm:text-4xl lg:text-4xl hover:text-[#FFB040] transition-all duration-700 cursor-pointer font-semibold '>Réalisations</li>
                 </Link>
                 <Link href="/contact">
-                  <li className=' text-2xl sm:text-4xl lg:text-4xl hover:text-[#FFB040] transition-all duration-700 cursor-pointer font-semibold '>Contact</li>
+                  <li className=' text-2xl text-white sm:text-4xl lg:text-4xl hover:text-[#FFB040] transition-all duration-700 cursor-pointer font-semibold '>Contact</li>
                 </Link>
                 
             </ul>
           </div>
           <div className="boxMedias p-10">
             <div className=" grid gap-3 mb-10">
-              <h4 className=' text-gray-500'>Medias  sociaux</h4>
-              <Link  href="https://github.com/michalsnik/aos"><p className='cursor-pointer hover:text-[#FFB040] transition-all duration-700'>Linkedin</p></Link>
-              <Link  href="https://github.com/michalsnik/aos"><p className='cursor-pointer hover:text-[#FFB040] transition-all duration-700'>Twitter</p></Link>
-              <Link  href="https://github.com/michalsnik/aos"><p className='cursor-pointer hover:text-[#FFB040] transition-all duration-700'>Facebook</p></Link>
+              <h4 className=' text-gray-400'>Medias  sociaux</h4>
+              <Link  href="https://github.com/michalsnik/aos"><p className='cursor-pointer text-white hover:text-[#FFB040] transition-all duration-700'>Linkedin</p></Link>
+              <Link  href="https://github.com/michalsnik/aos"><p className='cursor-pointer text-white hover:text-[#FFB040] transition-all duration-700'>Twitter</p></Link>
+              <Link  href="https://github.com/michalsnik/aos"><p className='cursor-pointer text-white hover:text-[#FFB040] transition-all duration-700'>Facebook</p></Link>
             </div>
             <div className='grid gap-3 mt-4'>
-              <h4 className=' text-gray-500'>Contactez-nous !</h4>
-              <div className='flex items-center cursor-pointer hover:text-[#FFB040] transition-all duration-700'>
+              <h4 className=' text-gray-400'>Contactez-nous !</h4>
+              <div className='text-white flex items-center cursor-pointer hover:text-[#FFB040] transition-all duration-700'>
                 <FaWhatsapp className='mr-2 '/> +1 514 974 1747
               </div>
-              <div className='flex items-center cursor-pointer hover:text-[#FFB040] transition-all duration-700'>
+              <div className='text-white flex items-center cursor-pointer hover:text-[#FFB040] transition-all duration-700'>
                 <MdOutlineEmail className='mr-2 '/> info@uempire.ca
               </div>
               
